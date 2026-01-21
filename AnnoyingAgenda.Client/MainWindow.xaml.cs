e0023eff56d5b0bf59667adec650719de4fea4fb
Sean de beeg <seandepew2011@gmail.com>
2026-01-21 18:59:37 -0500

    Change directory format

---
+++ AnnoyingAgenda.Client/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls;
+
+namespace AnnoyingAgenda.Client
+{
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void DragWindow(object sender, MouseButtonEventArgs e)
+    {
+      if (e.ChangedButton == MouseButton.Left)
+      {
+        this.DragMove();
+      }
+    }
+  }
+}
