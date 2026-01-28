3e4a57ba045d6aaa4c5a9cf1d27fc63539b0d113
Sean de beeg <seandepew2011@gmail.com>
2026-01-28 11:15:23 -0500

    Shared project created

---
+++ AnnoyingAgenda.Shared/Classes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnnoyingAgenda.Shared
+{
+  class ToDoList
+  {
+    public required string Name { get; set; }
+    public required string Purpose { get; set; }
+  }
+
+  class ToDoItem
+  {
+    public required string Name { get; set; }
+    public DateTime DueDate {get; set;}
+  }
+}
