2f95a64a14965b97e20f6f720b1190fb34034209
Sean de beeg <seandepew2011@gmail.com>
2026-01-31 12:50:21 -0500

    Buttery smooth animation on the Event buttons

--- AnnoyingAgenda.Client/ListEditor.xaml.cs
+++ AnnoyingAgenda.Client/ListEditor.xaml.cs
@@ -1,5 +1,10 @@
 ﻿using AnnoyingAgenda.Shared;
+using System.IO;
 using System.Windows.Controls;
+using System.Text.Json;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Media.Animation;
 
 namespace AnnoyingAgenda.Client
 {
@@ -7,11 +12,58 @@ namespace AnnoyingAgenda.Client
   {
     private MainWindow ParentWindow;
     private ToDoList CurrentList;
+    private List<ToDoList> AllLists;
     public ListEditor(MainWindow _parentWindow, ToDoList _currentList)
     {
       ParentWindow = _parentWindow;
       CurrentList = _currentList;
       InitializeComponent();
+
+      var JsonFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Annoying Agenda", "aa.json");
+
+      if (!File.Exists(JsonFilePath) || string.IsNullOrWhiteSpace(File.ReadAllText(JsonFilePath)))
+      {
+        File.WriteAllText(JsonFilePath, JsonSerializer.Serialize(new List<ToDoList>()));
+      }
+      else
+      {
+        AllLists = JsonSerializer.Deserialize<List<ToDoList>>(File.ReadAllText(JsonFilePath));
+        File.WriteAllText(JsonFilePath, JsonSerializer.Serialize(AllLists));
+      }
+    }
+
+    private void MouseOverAnimation(object sender, MouseEventArgs e)
+    {
+      Button HoveredButton = (Button)e.Source;
+
+      ThicknessAnimation LeftMarginAnimation = new()
+      {
+        From = new Thickness(0, 10, 0, 10),
+        To = new Thickness(0, 10, 20, 10),
+        Duration = TimeSpan.FromMilliseconds(100),
+      };
+      Storyboard.SetTargetProperty(LeftMarginAnimation, new PropertyPath("Margin"));
+      Storyboard.SetTarget(LeftMarginAnimation, HoveredButton);
+      Storyboard LeftMarginStoryBoard = new();
+      LeftMarginStoryBoard.Children.Add(LeftMarginAnimation);
+      LeftMarginStoryBoard.Begin();
+    }
+
+    private void MouseLeaveAnimation(object sender, MouseEventArgs e)
+    {
+      Button HoveredButton = (Button)e.Source;
+
+      ThicknessAnimation LeftMarginAnimation = new()
+      {
+        From = new Thickness(0, 10, 20, 10),
+        To = new Thickness(0, 10, 0, 10),
+        Duration = TimeSpan.FromMilliseconds(100),
+      };
+      Storyboard.SetTargetProperty(LeftMarginAnimation, new PropertyPath("Margin"));
+      Storyboard.SetTarget(LeftMarginAnimation, HoveredButton);
+      Storyboard LeftMarginStoryBoard = new();
+      LeftMarginStoryBoard.Children.Add(LeftMarginAnimation);
+      LeftMarginStoryBoard.Begin();
     }
   }
 }
