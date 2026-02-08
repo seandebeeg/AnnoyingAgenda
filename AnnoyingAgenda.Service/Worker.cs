9d26012b3b36e2d5cade63f6ed131e1abe6bcec2
Sean de beeg <seandepew2011@gmail.com>
2026-02-07 20:31:59 -0500

    General gist of what the service will be

--- AnnoyingAgenda.Service/Worker.cs
+++ AnnoyingAgenda.Service/Worker.cs
@@ -1,3 +1,7 @@
+using AnnoyingAgenda.Shared;
+using System.Text.Json;
+using Microsoft.Extensions.Options;
+
 namespace AnnoyingAgenda.Service
 {
   public class Worker : BackgroundService
@@ -7,13 +11,61 @@ namespace AnnoyingAgenda.Service
     public Worker(ILogger<Worker> logger)
     {
       _logger = logger;
+      _cancellationToken = cancellationToken.ApplicationStopping;
+
+      var SettingsJsonPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Annoying Agenda", "Settings.json");
+
+      if (!File.Exists(SettingsJsonPath) || string.IsNullOrWhiteSpace(File.ReadAllText(SettingsJsonPath)))
+      {
+        File.WriteAllText(SettingsJsonPath, JsonSerializer.Serialize(new Settings(), new JsonSerializerOptions() { WriteIndented = true }));
+        ServiceSettings = new Settings();
+      }
+      else
+      {
+        ServiceSettings = JsonSerializer.Deserialize<Settings>(File.ReadAllText(SettingsJsonPath)) ?? new Settings();
+        File.WriteAllText(SettingsJsonPath, JsonSerializer.Serialize(ServiceSettings));
+    }
+
+      var ListFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Annoying Agenda", "Lists.json");
+
+      if (!File.Exists(ListFilePath) || string.IsNullOrWhiteSpace(File.ReadAllText(ListFilePath)))
+      {
+        File.WriteAllText(ListFilePath, JsonSerializer.Serialize(new List<ToDoList>(), new JsonSerializerOptions() { WriteIndented = true }));
+      }
+      else
+      {
+        AllLists = JsonSerializer.Deserialize<List<ToDoList>>(File.ReadAllText(ListFilePath)) ?? new List<ToDoList>();
+      }
+
+      ServiceSettings.ServiceRootPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AnnoyingAgenda.Service.exe");
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+      bool HasOverdueTasks = false;
+
       while (!stoppingToken.IsCancellationRequested)
       {
-        if (_logger.IsEnabled(LogLevel.Information))
+        foreach(ToDoList List in AllLists)
+        {
+          foreach(ToDoItem Item in List.ListItems)
+          {
+            if (DateTime.Now >= Item.DueDate)
+            {
+              HasOverdueTasks = true;
+              _logger.LogInformation("Overdue Task: {Item}", Item.Name);
+            }
+          }
+        }
+
+        if (HasOverdueTasks)
+        {
+
+        }
+        await Task.Run(() => UpdateListsAndSettings());
+        await Task.Delay(3000);
+      }
+    }
         {
           _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
         }
