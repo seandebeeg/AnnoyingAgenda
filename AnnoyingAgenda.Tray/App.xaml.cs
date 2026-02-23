34cb170ecff7c188e6068591efc4d42313b44f58
Sean de beeg <seandepew2011@gmail.com>
2026-02-23 18:09:06 -0500

    Added a system tray app that will be used to notify the procrastinators

---
+++ AnnoyingAgenda.Tray/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows;
+using System.IO.Pipes;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+
+namespace AnnoyingAgenda.Tray
+{
+  public partial class App : System.Windows.Application
+  {
+    private NotifyIcon Tray = new();
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+      base.OnStartup(e);
+
+      Tray = new();
+
+      Tray.Icon = Icon.ExtractAssociatedIcon(System.Windows.Forms.Application.ExecutablePath);
+      Tray.Visible = true;
+      Tray.DoubleClick += (s, args) => Process.Start("AnnoyingAgenda.Client.exe"); 
+    }
+
+    NamedPipeClientStream ClientPipe = new(".", "AnnoyingAgenda", PipeDirection.In);
+  }
+}
---
+++ AnnoyingAgenda.Tray/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO.Pipes;
+namespace AnnoyingAgenda.Tray
+{
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      this.Hide();
+    }
+    private NamedPipeClientStream ClientPipe = new(".", "AnnoyingAgenda", PipeDirection.In);
+  }
+}
